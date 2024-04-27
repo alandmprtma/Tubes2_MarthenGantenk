a0137c4180827dfc702a6e811c21812838e71145
nerggg <13522147@std.stei.itb.ac.id>
2024-04-27 10:00:41 +0700

    fix: bfs title inconsistency

--- backend/bfs.go
+++ backend/bfs.go
@@ -37,7 +37,7 @@ func removeDuplicates(list []string) []string {
     result := []string{}
 
     for _, value := range list {
-        if encountered[value] == false {
+        if !encountered[value] {
             encountered[value] = true
             result = append(result, value)
         }
@@ -62,9 +62,12 @@ func bfsMultiCall(startURL string, targetTitle string) ([][]string, int, int, in
 	
 	var hrefs []string
 	var mu sync.Mutex
+	found := false
+	depth := 2
+	multiDepth := 10
 	
 	startTime := time.Now()
-	bfsMulti(startURL, targetTitle, hrefs, &mu, &articlesChecked, &results)
+	bfsMulti(startURL, targetTitle, depth, hrefs, &mu, &articlesChecked, &articlesTraversed, &found, &results, &multiDepth)
 	elapsedTime := time.Since(startTime).Seconds()
 	
 	for i, element := range results {
@@ -76,8 +79,7 @@ func bfsMultiCall(startURL string, targetTitle string) ([][]string, int, int, in
 	return results, articlesChecked, articlesTraversed, numberPath, elapsedTime
 } 
 
-func bfsMulti(startURL, targetTitle string, hrefs []string, mu *sync.Mutex, passed *int, results *[][]string) {
-	depth := 4
+func bfsMulti(startURL, targetTitle string, depth int, hrefs []string, mu *sync.Mutex, checked *int, traversed *int, found *bool, results *[][]string, multiDepth *int) {
 	queue := []Page{{URL: startURL, Path: []string{}, Depth: depth}}
 
 	for len(queue) > 0 {
@@ -96,51 +98,58 @@ func bfsMulti(startURL, targetTitle string, hrefs []string, mu *sync.Mutex, pass
 			go func(page Page) {
 				defer wg.Done()
 
-				if page.Depth == 0 {
+				if page.Depth > *multiDepth {
 					return
 				}
 
 				res, err := http.Get(page.URL)
 				if err != nil {
-					log.Fatal(err)
+					log.Print(err)
 				}
 				defer res.Body.Close()
 				if res.StatusCode != 200 {
-					log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+					fmt.Println(page.URL)
+					log.Printf("status code error: %d %s", res.StatusCode, res.Status)
+					return
 				}
 
 				doc, err := goquery.NewDocumentFromReader(res.Body)
 				if err != nil {
-					log.Fatal(err)
+					log.Print(err)
 				}
 
 				firstPath := doc.Find("title").Text()
 				firstPathSplit := strings.Split(firstPath, "-")
 				if len(firstPathSplit) > 0 {
+					*checked += page.Depth
 					page.Path = append(page.Path, strings.TrimSpace(firstPathSplit[0]))
 				}
 
-				// fmt.Println(strings.TrimSpace(firstPathSplit[0]))
-				
 				content := doc.Find("#mw-content-text")
 				content.Find("a").Each(func(i int, s *goquery.Selection) {
 					href, exists := s.Attr("href")
-					if exists && strings.HasPrefix(href, "/wiki/") {
-						title := strings.TrimSpace(firstPathSplit[0])
-							if title == targetTitle {
-								result := append(page.Path, title)
-								*results = append(*results, result)
-								// result := strings.Join(page.Path, " -> ")
-								// fmt.Printf("Path: %s -> %s\n", result, title)
-								return
-							} else if href != "/wiki/Main_Page" && !isInList(href, hrefs) {
-								hrefs = append(hrefs, href)
-								mu.Lock()
-								*passed++
-								queue = append(queue, Page{URL: "https://en.wikipedia.org" + href, Path: page.Path, Depth: page.Depth - 1})
-								mu.Unlock()
-							}
+					if exists && strings.HasPrefix(href, "/wiki/") && !strings.Contains(href, ":") {
+						// Extract the title from the URL
+						title := strings.TrimPrefix(href, "/wiki/")
+						title = strings.ReplaceAll(title, "_", " ") // Replace underscores with spaces
+						fmt.Printf("checking %v\n", page.Path)
+						if title == targetTitle {
+							result := append(page.Path, title)
+							*results = append(*results, result)
+							*found = true
+							// fmt.Printf("Path: %s -> %s\n", result, title)
+							// fmt.Printf("Path Length: %d\n", page.Depth)
+							// fmt.Printf("Checked Article: %d\n", *checked)
+							*multiDepth = page.Depth
+							return
+						} else if href != "/wiki/Main_Page" && !isInList(href, hrefs) {
+							hrefs = append(hrefs, href)
+							mu.Lock()
+							*traversed++
+							queue = append(queue, Page{URL: "https://en.wikipedia.org" + href, Path: page.Path, Depth: page.Depth + 1})
+							mu.Unlock()
 						}
+					}
 				})
 			}(currentPage)
 		}
@@ -156,9 +165,10 @@ func bfsSingleCall(startURL string, targetTitle string) ([][]string, int, int, i
 	var hrefs []string
 	var mu sync.Mutex
 	found := false
+	depth := 2
 	
 	startTime := time.Now()
-	bfsSingle(startURL, targetTitle, hrefs, &mu, &articlesChecked, &found, &results)
+	bfsSingle(startURL, targetTitle, depth, hrefs, &mu, &articlesChecked, &articlesTraversed, &found, &results)
 	elapsedTime := time.Since(startTime).Seconds()
 	
 	for i, element := range results {
@@ -170,12 +180,10 @@ func bfsSingleCall(startURL string, targetTitle string) ([][]string, int, int, i
 	return results, articlesChecked, articlesTraversed, numberPath, elapsedTime
 } 
 
-func bfsSingle(startURL, targetTitle string, hrefs []string, mu *sync.Mutex, passed *int, found *bool, results *[][]string) {
-	depth := 2
+func bfsSingle(startURL, targetTitle string, depth int, hrefs []string, mu *sync.Mutex, checked *int, traversed *int, found *bool, results *[][]string) {
 	queue := []Page{{URL: startURL, Path: []string{}, Depth: depth}}
 
 	for len(queue) > 0 {
-
 		mu.Lock()
 		currentPages := make([]Page, 0, 100)
 		for i := 0; i < 100 && len(queue) > 0; i++ {
@@ -183,57 +191,61 @@ func bfsSingle(startURL, targetTitle string, hrefs []string, mu *sync.Mutex, pas
 			queue = queue[1:]
 		}
 		mu.Unlock()
-
+		
 		var wg sync.WaitGroup
 		for _, currentPage := range currentPages {
 			wg.Add(1)
 			go func(page Page) {
 				defer wg.Done()
-
+				
 				if *found {
 					return
 				}
 
 				res, err := http.Get(page.URL)
 				if err != nil {
-					log.Fatal(err)
+					log.Print(err)
 				}
 				defer res.Body.Close()
 				if res.StatusCode != 200 {
-					log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+					log.Printf("status code error: %d %s", res.StatusCode, res.Status)
+					return
 				}
 
 				doc, err := goquery.NewDocumentFromReader(res.Body)
 				if err != nil {
-					log.Fatal(err)
+					log.Print(err)
 				}
-
+				
 				firstPath := doc.Find("title").Text()
-				firstPathSplit := strings.Split(firstPath, " - ")
+				firstPathSplit := strings.Split(firstPath, "-")
 				if len(firstPathSplit) > 0 {
+					*checked += page.Depth
 					page.Path = append(page.Path, strings.TrimSpace(firstPathSplit[0]))
 				}
-
+				
 				content := doc.Find("#mw-content-text")
 				content.Find("a").Each(func(i int, s *goquery.Selection) {
 					href, exists := s.Attr("href")
-					if exists && strings.HasPrefix(href, "/wiki/") {
-						title := firstPathSplit[0]
-						// fmt.Printf("checking %s\n", title)
+					if exists && strings.HasPrefix(href, "/wiki/") && !strings.Contains(href, ":") {
+						// Extract the title from the URL
+						title := strings.TrimPrefix(href, "/wiki/")
+						title = strings.ReplaceAll(title, "_", " ") // Replace underscores with spaces
+						fmt.Printf("checking %v\n", page.Path)
 						if title == targetTitle {
 							result := append(page.Path, title)
 							*results = append(*results, result)
 							*found = true
 							// fmt.Printf("Path: %s -> %s\n", result, title)
 							// fmt.Printf("Path Length: %d\n", page.Depth)
-							// fmt.Printf("Checked Article: %d\n", *passed)
+							// fmt.Printf("Checked Article: %d\n", *checked)
 							return
-						} else if href != "/wiki/Main_Page" && !isInList(href, hrefs) {
-							hrefs = append(hrefs, href)
-							mu.Lock()
-							*passed++
-							queue = append(queue, Page{URL: "https://en.wikipedia.org" + href, Path: page.Path, Depth: page.Depth + 1})
-							mu.Unlock()
+							} else if href != "/wiki/Main_Page" && !isInList(href, hrefs) {
+								hrefs = append(hrefs, href)
+								mu.Lock()
+								*traversed++
+								queue = append(queue, Page{URL: "https://en.wikipedia.org" + href, Path: page.Path, Depth: page.Depth + 1})
+								mu.Unlock()
 						}
 					}
 				})
